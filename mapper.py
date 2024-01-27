c80e8060f904bcf3fffb6f98902498f164e28303
Michael Evans <canastotacoach@gmail.com>
2024-01-27 18:42:04 +0000

    added more stopwords

--- mapper.py
+++ mapper.py
@@ -4,11 +4,18 @@ import sys
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line1 = line.strip()
+
+    # Make all lines lowercase
+    line2 = line1.lower()
 
     # split the line into words; splits on any whitespace
-    words = line.split()
+    words = line2.split()
+
+    # Add Stopwords
+    stopwords = set(['the', 'and', ',' ,'.', 'i', 'you', 'we'])
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if not word in stopwords:
+            print '%s\t%s' % (word, "1")
